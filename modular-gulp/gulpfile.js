be07d245d191ea8fc998a5cc4fbe0abd0eb9f04a
Iris Ibekwe <mibekwe@cox.net>
2019-09-26 00:08:44 -0400

    Update the gulpfile.

--- modular-gulp/gulpfile.js
+++ modular-gulp/gulpfile.js
@@ -1,31 +1,31 @@
+var gulp = require('gulp');
 
-/* -------------------------------
- Available Tasks
- ------------------------------- */
-// gulp                     -- Lints js & sass, watches sass, starts up the PL server.
-// gulp build               -- Task for frontend:build. Lints JS & Sass, compiles sass & generates patternlab.
-// gulp sass                -- Compiles Sass Files.
-// gulp sass-lint           -- Lints Sass Files.
-// gulp patternlab:watch    -- Starts the PatternLab servers and watches tasks.
-// gulp patternlab:generate -- Generates PatternLab.
-// gulp watch               -- Lints & Watches sass changes .
-
-var gulp    = require('gulp');
+// == PLUGINS ====
+/** This uses the gulp load plugin npm library to avoid having to declare
+ * each plugin you use with "var plugin = require('gulp-plugin`)".
+ * It also lazy-loads plugins depending on what is being used by the task being run.
+ * NPM: https://www.npmjs.com/package/gulp-load-plugins
+ * Learn more: https://andy-carter.com/blog/automatically-load-gulp-plugins-with-gulp-load-plugins 
+ */
 
 var plugins = require('gulp-load-plugins')({
   pattern: ['*', 'gulp-*', '@*/gulp{-,.}*'],
   rename: {
-    'gulp-autoprefixer': 'prefix',
-    'css-mqpacker'     : 'mqpacker',
-    'gulp-sass-glob'   : 'sassglob'
+    'gulp-autoprefixer'            : 'prefix', // Adds browser prefixes where needed
+    'gulp-group-css-media-queries' : 'gcmq',   // This will group media queries together in compiled CSS.
+    'gulp-sass-glob'               : 'sassglob', // Allows you to compile all sass partials under a given directory
+    'gulp-eslint'                  : 'eslint', // Lints JS syntax
+    'postcss-clean'                : 'clean', // Minifies & cleans up CSS
+    'gulp-sourcemaps'              : 'sourcemaps'
   }
 });
 
-// Directories
-var sassFiles       = 'pattern-lab/source/sass/**/*.scss'; // replace this with the path to your sass directory.
-var patternLabFiles = 'pattern-lab/source/_patterns/**/*.scss'; // Replace with top level PL directory if any
-var cssDir          = 'pattern-lab/source/css'; // Replace with path to CSS dir
-var jsDir           = 'js/*.js';
+// == DIRECTORIES ==
+var sassFiles       = 'source/sass/**/*.scss'; // replace this with the path to your sass directory.
+var patternLabFiles = 'source/_patterns/**/*.scss'; // Replace with top level PL directory if any
+var cssDir          = 'source/css'; // Replace with path to CSS dir
+var optimizedJSDir  = 'components/js'; // Directory for minified JS files
+var jsDir           = 'components/js/src/*.js'; // Source directory for JS
 
 'use strict';
 var options = {
@@ -46,13 +46,10 @@ var options = {
     jsFiles: jsDir
   }
 };
-
-
 // Tasks
-require('./gulp-tasks/default')(gulp, options, plugins);
 require('./gulp-tasks/sass')(gulp, options, plugins);
-require('./gulp-tasks/sass-lint')(gulp, options, plugins);
-require('./gulp-tasks/js-lint')(gulp, options, plugins);
-require('./gulp-tasks/pattern-lab')(gulp, options, plugins);
-require('./gulp-tasks/watch')(gulp, options, plugins);
-require('./gulp-tasks/build')(gulp, options, plugins);
+// Commented these out until their task JS files have been updated.
+// require('./gulp-tasks/sass-lint')(gulp, options, plugins);
+// require('./gulp-tasks/js-lint')(gulp, options, plugins);
+// require('./gulp-tasks/js-optimize')(gulp, options, plugins);
+// require('./gulp-tasks/watch')(gulp, options, plugins);
